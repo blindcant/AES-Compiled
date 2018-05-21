41ea463f8fdd501350f0f96e83eb04626a02ad37
blindcant <27213400+blindcant@users.noreply.github.com>
2018-05-21 20:27:28 +1000

    Updated the install file to include details for both encrypt and decrypt.

--- Teradata_Java_UDF_install.sql
+++ Teradata_Java_UDF_install.sql
@@ -1,19 +1,43 @@
-.logon teradata_server/teradata_user,;
-database my_database_name ;
-drop  FUNCTION my_database_name.MY_FUCTION_NAME;
-call sqlj.remove_jar('my_java_file_teradata_name', 0);
-
- 
-
-call sqlj.install_jar('CJ?C:\path\to\source_code\my_java_file.jar','my_java_file_teradata_name',0);
-
-call SQLJ.REDISTRIBUTE_JAR('my_java_file_teradata_name');
-
- 
-CREATE FUNCTION my_database_name.MY_FUCTION_NAME (ciphertext varchar(27000) character set latin, iv char(16))
-RETURNS varchar(10000) character set unicode
+--Login
+.logon teradata_server/teradata_user
+;
+--Change database
+DATABASE my_database_name
+;
+--Drop existing encrypt function and the existing jar file
+DROP FUNCTION my_database_name.AES_Encrypt
+;
+CALL SQLJ.REMOVE_JAR('my_java_file_teradata_name', 0)
+;
+--Install the jar file
+CALL SQLJ.INSTALL_JAR('CJ?C:\path\to\source_code\my_java_file.jar','my_java_file_teradata_name',0)
+;
+CALL SQLJ.REDISTRIBUTE_JAR('my_java_file_teradata_name')
+;
+--Create the encrypt function. It should accept unicode (it accepts utf-8 but Java internally is utf-16) and return ASCII (base64).
+CREATE FUNCTION my_database_name.AES_Encrypt(plaintext varchar(27000) character set latin, secretKey varchar(32))
+RETURNS varchar(13500) character set ASCII
 LANGUAGE JAVA
 NO SQL
 PARAMETER STYLE JAVA
 RETURNS NULL ON NULL INPUT
-EXTERNAL NAME 'my_java_file_teradata_name:my_java_file_class_name.my_java_file_method_name';
+EXTERNAL NAME 'my_java_file_teradata_name:my_java_file_class_name.my_java_file_method_name'
+;
+--Repeat for the Decrypt function
+DROP FUNCTION my_database_name.AES_Decrypt
+;
+CALL SQLJ.REMOVE_JAR('my_java_file_teradata_name', 0)
+;
+CALL SQLJ.INSTALL_JAR('CJ?C:\path\to\source_code\my_java_file.jar','my_java_file_teradata_name',0)
+;
+CALL SQLJ.REDISTRIBUTE_JAR('my_java_file_teradata_name')
+;
+--It should accept ASCII (base64) and return unicode (it returns utf-8 but Teradata internally is utf-16)
+CREATE FUNCTION my_database_name.AES_Decrypt(ciphertext varchar(13500) character set ASCII, secretKey varchar(32))
+RETURNS varchar(27000) character set unicode
+LANGUAGE JAVA
+NO SQL
+PARAMETER STYLE JAVA
+RETURNS NULL ON NULL INPUT
+EXTERNAL NAME 'my_java_file_teradata_name:my_java_file_class_name.my_java_file_method_name'
+;
